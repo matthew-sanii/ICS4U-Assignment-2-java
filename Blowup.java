bfe088d363399538357149c7aab0fe50ae2d379c
Ubuntu <ubuntu@ip-172-31-89-75.ec2.internal>
2021-12-05 18:30:54 +0000

    edited Blowup.java

--- Blowup.java
+++ Blowup.java
@@ -16,6 +16,11 @@ import java.util.Scanner;
 final class Blowup {
 
     /**
+    * The number of times the first value is copied
+    */
+    private static final int copyTimes = 0;
+
+    /**
     * Prevent instantiation
     * Throw an exception IllegalStateException.
     * if this ever is called
@@ -27,23 +32,32 @@ final class Blowup {
         throw new IllegalStateException("Cannot be instantiated");
     }
 
-    public static String blowup(String str; Int amount) {
+    public static String blowup(String str, int amount) {
         final String result;
         final int times;
         if (str.isEmpty()) {
             result = "";
         }
         else {
-        for (int copy = 0; copy != amount; copy++) {
-            result += str(0);
-        }
+            for (int copy = 0; copy != amount; copy++) {
+                result += str(0);
+            }
             try {
                 times = Integer.valueOf(str(0));
             }
             catch (NumberFormatException errorCode) {
                 times = 0;
             }
-
+            result = blowup(str.substring(1), times);
         }
-}
+        return result;
+    }
 
+    public static void main(final String[] args) {
+        final Scanner userInput = new Scanner(System.in);
+        System.out.print("input the string you want to 'blowup': ");
+        final String input = userInput.nextLine();
+        final String answer = blowup(input, copyTimes);
+        System.out.println("The blownup string is: " + answer);
+}
+}
