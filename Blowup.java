7285f6ab1f9be122389fce043950450fabde9623
Ubuntu <ubuntu@ip-172-31-89-75.ec2.internal>
2021-12-05 20:31:19 +0000

    edited Blowup.java

--- Blowup.java
+++ Blowup.java
@@ -1,24 +1,26 @@
 /*
-* The reverse program.
+* The Blowup program, that turns a string into a new string
+* Using the int placements to make the next string character
+* appear multiple times.
 *
 * @author Matthew Sanii
 * @version 1.0
-* @since 2021-29-11
+* @since 2021-12-05
 *
 */
 
 import java.util.Scanner;
 
 /**
-* This is the reverse program.
+* This is the blowup program.
 */
 
 final class Blowup {
 
     /**
-    * The number of times the first value is copied
+    * The number of times the first value is copied.
     */
-    private static final int copyTimes = 0;
+    private static final int COPYTIMES = 0;
 
     /**
     * Prevent instantiation
@@ -32,32 +34,52 @@ final class Blowup {
         throw new IllegalStateException("Cannot be instantiated");
     }
 
+    /**
+    * Expands and alters the string given based on int placements.
+    *
+    * @param str the user's string input.
+    * @param amount the number of times a specific character will appear.
+    * @return the blownup string.
+    */
     public static String blowup(String str, int amount) {
-        final String result;
-        final int times;
+        String result = "";
+        String results = "";
+        int copies = amount;
+        int multiplied;
         if (str.isEmpty()) {
             result = "";
         }
         else {
-            for (int copy = 0; copy != amount; copy++) {
-                result += str(0);
+            final char spot = str.charAt(0);
+            while (copies != 0) {
+                results += spot;
+                copies -= 1;
             }
             try {
-                times = Integer.valueOf(str(0));
+                multiplied = Character.getNumericValue(spot);
             }
             catch (NumberFormatException errorCode) {
-                times = 0;
+                multiplied = 0;
             }
-            result = blowup(str.substring(1), times);
+            result = blowup(str.substring(1), multiplied) + results;
         }
         return result;
     }
 
+    /**
+    * The starting main() function.
+    *
+    * @param args Name of file containing a string of numbers
+    */
     public static void main(final String[] args) {
         final Scanner userInput = new Scanner(System.in);
         System.out.print("input the string you want to 'blowup': ");
         final String input = userInput.nextLine();
-        final String answer = blowup(input, copyTimes);
+        if ("".equals(input)) {
+            System.out.println("Error, requires input.");
+            System.exit(0);
+        }
+        final String answer = blowup(input, COPYTIMES);
         System.out.println("The blownup string is: " + answer);
-}
+    }
 }
